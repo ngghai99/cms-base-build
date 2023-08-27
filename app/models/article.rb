f39670037998caa949cfd81174d85d12e6414afb
ngghung2311 <ngghung2311@gmail.com>
2023-08-27 08:21:21 +0700

    update view index

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true
+  enum status: [:active, :archived]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '', to: 'dashboard#index', as: 'admin_dashboard_root'
-
     resource :dashboard
     resources :articles
   end
