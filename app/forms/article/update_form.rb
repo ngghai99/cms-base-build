5cd3ccb11e9b6c438d934d2b30aa1998b78d54e4
ngghai <hai@klabs.vn>
2023-10-06 17:25:38 +0700

    update

--- app/forms/article/update_form.rb
+++ app/forms/article/update_form.rb
@@ -1,18 +1,20 @@
-class Article::UpdateForm
+class Article::CreateForm
   include ActiveModel::Model
-  attr_accessor :title, :content, :images, :status, :slug, :order, :article_catalogue_id
+  attr_accessor :title, :content, :images, :status, :slug, :order, :article_catalogue_id, :params
 
   validates :title, presence: true
   validates :content, presence: true
 
   def initialize(params={})
     @params = params
-    super(params)
+
+    return unless params[:article_create_form].present?
+    super(article_params)
   end
 
   def save
     if valid?
-      update_article
+      create_article
     else
       false
     end
@@ -20,7 +22,11 @@ class Article::UpdateForm
 
   private
 
-  def update_article
-    Article.update!(@params)
+  def article_params
+    params.require(:article_create_form).permit(:title, :content, :image, :status, :description, :article_catalogue_id, images: [])
+  end
+
+  def create_article
+    Article.create!(article_params)
   end
 end
