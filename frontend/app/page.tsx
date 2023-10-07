fcbcbc8291f9c9a816feb687ef396c678f255da2
ngghai <hai@klabs.vn>
2023-10-07 20:31:45 +0700

    update frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,32 +1,8 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import React from 'react'
 
 export default function Home() {
   return (
     <>
-      <meta charSet="utf-8" />
-      <title>Reporter - HTML Blog Template</title>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=5"
-      />
-      <meta name="description" content="This is meta description" />
-      <meta name="author" content="Themefisher" />
-      <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
-      <link rel="icon" href="images/favicon.png" type="image/x-icon" />
-      {/* theme meta */}
-      <meta name="theme-name" content="reporter" />
-      {/* # Google Fonts */}
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Neuton:wght@700&family=Work+Sans:wght@400;500;600;700&display=swap"
-        rel="stylesheet"
-      />
-      {/* # CSS Plugins */}
-      <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css" />
-      {/* # Main Style Sheet */}
-      <link rel="stylesheet" href="css/style.css" />
       <header className="navigation">
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light px-0">
