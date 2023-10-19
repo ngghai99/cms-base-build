395faf9d30ef38ca39deec81842ebea002927e9b
ngghai <hai@klabs.vn>
2023-10-19 15:53:08 +0700

    update

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,48 +1,50 @@
 class Admin::UsersController < ApplicationController
   before_action :set_admin_user, only: [:show, :edit, :update, :destroy]
+  before_action :permission_required, only: [:edit, :update, :destroy]
 
   def index
-    @admin_users = User.all.filter_by(params).order(created_at: :asc).decorate
+    @admin_clients = Client.all.filter_by(params).order(created_at: :asc).decorate
   end
 
   def show
   end
 
   def new
-    @user_form = User::CreateForm.new
+    @client_form = User::CreateForm.new
   end
 
   def create
-    @user_form = User::CreateForm.new(params)
+    @client_form = User::CreateForm.new(params)
     handle_form_save('users.save_article_successfully')
   end
 
   def edit
-    @user_form = User::UpdateForm.new(@admin_user)
+    @client_form = User::UpdateForm.new(@admin_client)
   end
 
   def update
-    if current_user == @admin_user || @admin_user.is_client?
-      @user_form = User::UpdateForm.new(params)
-      handle_form_save('users.update_article_successfully')
-    else
-      redirect_to admin_users_url, notice: I18n.t('permission.You_do_not_have_permission_to_update_other_users.')
-    end
+    @client_form = User::UpdateForm.new(params)
+    handle_form_save('users.update_article_successfully')
   end
 
   def destroy
-    @admin_user.destroy
+    @admin_client.destroy
     redirect_to admin_users_url, notice: I18n.t('users.destroyed_successfully!')
   end
 
   private
 
   def set_admin_user
-    @admin_user = User.find(params[:id])
+    @admin_client = Client.find(params[:id])
+  end
+
+  def permission_required
+    redirect_to admin_users_url, notice: I18n.t('permission.You_do_not_have_permission_to_update_other_users.') unless @admin_client.check_permissions(current_user)
+    true
   end
 
   def handle_form_save(success_message)
-    if @user_form.save
+    if @client_form.save
       redirect_to admin_users_path, notice: I18n.t(success_message)
     else
       render :new
