730334d55462105dedfc02a82a3d78b931226853
ngghai <hai@klabs.vn>
2023-10-20 22:10:53 +0700

    update API

--- frontend/components/ListLatestArticles.tsx
+++ frontend/components/ListLatestArticles.tsx
@@ -1,12 +1,10 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import api from '../utils/api';
-import baseURL from '../utils/api';
 
 export default function LatestArticles() {
   const [articles, setArticles] = useState([]);
   const API_URL = process.env.API_URL
-  console.log(API_URL)
 
   useEffect(() => {
     async function fetchData() {
@@ -23,47 +21,80 @@ export default function LatestArticles() {
   return (
     <>
       <div className="col-12">
-        <h2 className="section-title">{}</h2>
+        <h2 className="section-title">Latest Articles</h2>
       </div>
       <div className="col-lg-8 mb-5 mb-lg-0">
         <div className="row">
           {
-            articles.map(article =>
-              <div className="col-md-6 mb-4">
-              <article className="card article-card article-card-sm h-100">
-                <a href="article.html">
-                  <div className="card-image">
-                    <div className="post-info">
-                      {" "}
-                      <span className="text-uppercase">03 Jun 2021</span>
-                      <span className="text-uppercase">2 minutes read</span>
+            articles.map((article, index) =>
+              index === 0 ? (
+                <div key={article.id} className="col-12 mb-4">
+                  <article className="card article-card">
+                    <a href="article.html">
+                      <div className="card-image">
+                        <div className="post-info">
+                          <span className="text-uppercase">04 Jun 2021</span>
+                          <span className="text-uppercase">3 minutes read</span>
+                        </div>
+                        {article.images[0] && <img loading="lazy" src={API_URL + article.images[0].url} alt="Image" decoding="async" className="w-100 h-100" />}
+                      </div>
+                    </a>
+                    <div className="card-body px-0 pb-1">
+                      <ul className="post-meta mb-2">
+                        <li>
+                          <a href="#!">travel</a>
+                          <a href="#!">news</a>
+                        </li>
+                      </ul>
+                      <h2 className="h1">
+                        <a className="post-title" href="article.html">
+                          {article.title}
+                        </a>
+                      </h2>
+                      <div className='small-text-description' dangerouslySetInnerHTML={{ __html: article.mini_description?.substring(0, 250) + "..."}}></div>
+                      <div className="content">
+                        <a className="read-more-btn" href="article.html">Read Full Article</a>
+                      </div>
                     </div>
-                    {article.images[0] && <img loading="lazy" src={API_URL + article.images[0].url} alt="Image" decoding="async" className="w-100 img-card-list" />}
-                  </div>
-                </a>
-                <div className="card-body px-0 pb-0">
-                  <ul className="post-meta mb-2">
-                    <li>
-                      {" "}
-                      <a href="#!">travel</a>
-                    </li>
-                  </ul>
-                  <h2>
-                    <a className="post-title" href="article.html">
-                      {article.title}
-                    </a>
-                  </h2>
-                  <div className='small-text-description' dangerouslySetInnerHTML={{ __html: article.content.substring(0, 200)}}></div>
-                  <div className="content">
-                    {" "}
-                    <a className="read-more-btn" href="article.html">
-                      Read Full Article
-                    </a>
-                  </div>
+                  </article>
                 </div>
-              </article>
-              </div>
+              ) : (
+                <div key={article.id} className="col-md-6 mb-4">
+                  <article className="card article-card article-card-sm h-100">
+                  <a href="article.html">
+                    <div className="card-image">
+                      <div className="post-info">
+                        {" "}
+                        <span className="text-uppercase">03 Jun 2021</span>
+                        <span className="text-uppercase">2 minutes read</span>
+                      </div>
+                      {article.images[0] && <img loading="lazy" src={API_URL + article.images[0].url} alt="Image" decoding="async" className="w-100 img-card-list" />}
+                    </div>
+                  </a>
+                  <div className="card-body px-0 pb-0">
+                    <ul className="post-meta mb-2">
+                      <li>
+                        {" "}
+                        <a href="#!">travel</a>
+                      </li>
+                    </ul>
+                    <h2>
+                      <a className="post-title" href="article.html">
+                        {article.title}
+                      </a>
+                    </h2>
+                    <div className='small-text-description' dangerouslySetInnerHTML={{ __html: article.mini_description?.substring(0, 70) + "..."}}></div>
+                    <div className="content">
+                      {" "}
+                      <a className="read-more-btn" href="article.html">
+                        Read Full Article
+                      </a>
+                    </div>
+                  </div>
+                  </article>
+                </div>
               )
+            )
           }
           <div className="col-12">
             <div className="row">
