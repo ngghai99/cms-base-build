9a770163a8e1953c7ef97cf366f42c3b0cead62c
ngghai <hai@klabs.vn>
2023-10-24 15:36:47 +0700

    update fix css js

--- frontend/components/ArticleDetail.tsx
+++ frontend/components/ArticleDetail.tsx
@@ -1,10 +1,20 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-import Link from "next/link"
 import api from '../utils/api';
 
 export default function ArticleDetail({ArticleId}) {
   const [article, setArticle] = useState([]);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString.created_at)
+
+    const day = date.toLocaleString('en-us', { weekday: 'long' });
+    const month = date.toLocaleString('en-us', { month: 'short' });
+    const dayOfMonth = date.getDate();
+    const year = date.getFullYear();
+    const formatted = `${day} ,${month} ${dayOfMonth}, ${year}`;
+    return formatted
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -20,7 +30,13 @@ export default function ArticleDetail({ArticleId}) {
 
   return (
     <>
-      <div class="col-lg-8 mb-5 mb-lg-0">
+      <div className="col-lg-8 mb-5 mb-lg-0">
+        <h1>
+          {article.title}
+        </h1>
+        <p>
+          { formatDate(article) }
+        </p>
         <div className='small-text-description' dangerouslySetInnerHTML={{ __html: article.content }}>
         </div>
       </div>
